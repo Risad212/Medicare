cb10c07c44cb057be9de14c300a13ce2a2b10f7a
Risad212 <hafez.risad@gmail.com>
2023-10-25 15:47:36 +0600

    done

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,23 @@ document.addEventListener('click', function (e) {
     // Hamburger menu
     if (e.target.classList.contains('hamburger-toggle')) {
         e.target.children[0].classList.toggle('active');
-     }
+    }
 })
+
+
+// For Nav sticky
+const nav = document.querySelector('.header');
+let navTop = nav.offsetTop;
+function fixedNav() {
+    if (window.scrollY >= navTop) {
+        nav.classList.add('sticky-nav');
+    } else {
+        nav.classList.remove('sticky-nav');
+    }
+}
+window.addEventListener('scroll', fixedNav);
+
+
+
+
+
