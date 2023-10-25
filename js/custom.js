f52059e85bc5d0e1687f560dd5a20f51f7175a37
Risad212 <hafez.risad@gmail.com>
2023-10-25 12:41:07 +0600

    done

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// No JavaScript needed for this example anymore!
+document.addEventListener('click', function (e) {
+    // Hamburger menu
+    if (e.target.classList.contains('hamburger-toggle')) {
+        e.target.children[0].classList.toggle('active');
+     }
+})
