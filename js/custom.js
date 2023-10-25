e0f739701d27abeffe71184dd327064db67bf1e7
Risad212 <hafez.risad@gmail.com>
2023-10-25 18:29:11 +0600

    done

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,36 @@
-// No JavaScript needed for this example anymore!
-document.addEventListener('click', function (e) {
-    // Hamburger menu
-    if (e.target.classList.contains('hamburger-toggle')) {
-        e.target.children[0].classList.toggle('active');
-    }
-})
+(function ($) {
+    "use strict";
 
-/*[ Back to top ]
-===========================================================*/
-$(".scrollup").hide();
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
-        $('.scrollup').fadeIn();
-    } else {
-        $('.scrollup').fadeOut();
-    }
-});
-$('.scrollup').on('click', function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 800);
-    return false;
-});
+    let $window = $(window),
+        $body = $('body');
+
+    // No JavaScript needed for this example anymore!
+    document.addEventListener('click', function (e) {
+        // Hamburger menu
+        if (e.target.classList.contains('hamburger-toggle')) {
+            e.target.children[0].classList.toggle('active');
+        }
+    })
+
+    /*[ Back to top ]
+    ===========================================================*/
+    $(".scrollup").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').on('click', function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+
+})(jQuery);
 
 
 
